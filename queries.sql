a3b997c2312843a87af3780c6274440819f31897
hesam <hesam@LAPTOP-K4EHTU8F.localdomain>
2023-04-30 18:21:54 -0700

    add db and query files

---
+++ queries.sql
@@ -0,0 +1,38 @@
+-- write your queries here
+
+-- Join the two tables so that every column and record appears, regardless of if there is not an owner_id
+SELECT * FROM owners o
+FULL JOIN vehicles v
+ON o.id = v.owner_id;
+
+
+SELECT o.first_name, o.last_name, COUNT(v.owner_id)
+FROM owners o
+JOIN vehicles v
+ON o.id = v.owner_id
+GROUP BY (o.first_name, o.last_name)
+ORDER BY o.first_name ASC;
+
+
+-- This works too withou having "o." 
+-- N.B. first_name and last_name should be called in the aggregate function. look at the GROUP BY! 
+
+-- SELECT first_name, last_name, COUNT(v.owner_id)
+-- FROM owners o
+-- JOIN vehicles v
+-- ON o.id = v.owner_id
+-- GROUP BY (first_name, last_name)
+-- ORDER BY first_name ASC;
+
+
+-- Count the number of cars for each owner and display the average price for each of the cars as integers.
+-- Display the owners first_name , last_name, average price and count of vehicles. 
+-- The first_name should be ordered in descending order.
+-- Only display results with more than one vehicle and an average price greater than 10000
+SELECT o.first_name, o.last_name, ROUND (AVG(price)) AS average_price, COUNT(v.owner_id) AS num_vehicles
+FROM owners o
+JOIN vehicles v
+ON o.id = v.owner_id
+GROUP BY (o.first_name, o.last_name)
+HAVING COUNT(v.owner_id) > 1 AND ROUND (AVG(price)) > 10000
+ORDER BY o.first_name DESC;
